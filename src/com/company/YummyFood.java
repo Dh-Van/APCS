7e8f3e0f26d399ebefb516d01b7a4eb473ac6e73
Dh-Van <dhvan17shah@gmail.com>
2021-09-10 11:33:49 -0400

    Added new class

---
+++ src/com/company/YummyFood.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class YummyFood {
+
+    public static void main(String[] args) {
+        System.out.print("YUMMY YUMMY ");
+        System.out.println("HI HI HI HI");
+        chocolate();
+        iceCream();
+    }
+
+    public static void chocolate(){
+        System.out.println("Chocolate");
+    }
+
+    public static void iceCream(){
+        System.out.println("Vanilla");
+        chocolate();
+        System.out.println("That was yummy");
+    }
+}
