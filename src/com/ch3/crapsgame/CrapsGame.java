9517347bf1de4ec3e9552ae394b27441eb6cd248
Dh-Van <dhvan17shah@gmail.com>
2021-11-01 11:49:35 -0400

    Quick Fix

--- src/com/ch3/crapsgame/CrapsGame.java
+++ src/com/ch3/crapsgame/CrapsGame.java
@@ -15,21 +15,22 @@ public class CrapsGame
   public int processRoll(int total) {
     int result = 0;
 
-    if (point == 0 && total == 11) {
+    if (point == 0) {
+      if(total == 7 || total == 11){
+        point = 0;
+        result = 1;
+      } else if (total == 2 || total == 3 || total == 12){
+        point = 0;
+        result = -1;
+      } else {
+        point = total;
+      }
+    } else if(point == total) {
       point = 0;
       result = 1;
-    } else if (point == 0 && (total == 2 || total == 3 || total == 12)) {
+    } else if(total == 7){
       point = 0;
       result = -1;
-    } else if (point == 0){
-      point = total;
-    } else if (total == 7){
-      point = 0;
-      result = -1; //
-    }
-    else if(point == total){
-      point = 0;
-      return 1;
     }
     return result;
   }
