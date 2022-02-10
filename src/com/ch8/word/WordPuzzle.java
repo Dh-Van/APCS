9de216de0b7cc65d6176dc87ab69746e85dc6a2e
dh-van <dhvan17shah@gmail.com>
2022-02-10 10:57:11 -0500

    TO EZ

--- src/com/ch8/word/WordPuzzle.java
+++ src/com/ch8/word/WordPuzzle.java
@@ -22,11 +22,10 @@ public class WordPuzzle {
      * Precondition: r and c are valid indexes in blackBoxes
      */
     public boolean toBeLabeled(int r, int c, boolean [][] blackBoxes){
-        /* to be implemented in part a */
-        if(r = 0)
-
-
-        return false;    // replace this
+        if(blackBoxes[r][c]) return false;
+        if(r == 0 || c == 0) return true;
+        if(blackBoxes[r-1][c] || blackBoxes[r][c-1]) return true;
+        return false;
     }
 
     /* Write the WordPuzzle Constructor.  The constructor should initialize the
@@ -47,9 +46,24 @@ public class WordPuzzle {
      * @param blackBoxes - a 2D array of Boxes
      */
     public WordPuzzle(boolean [][] blackBoxes){
+        puzzle = new Box[blackBoxes.length][blackBoxes[0].length];
         for(int i = 0; i < blackBoxes.length; i++){
             for(int j = 0; j < blackBoxes[0].length; j++){
                 puzzle[i][j] = new Box(blackBoxes[i][j], 0);
+            }
+        }
+
+        int counter = 1;
+        for(int i = 0; i < puzzle.length; i++){
+            for(int j = 0; j < puzzle[0].length; j++){
+                if(toBeLabeled(i, j, blackBoxes)){
+                    puzzle[i][j] = new Box(false, counter);
+                    counter++;
+                } else if(!blackBoxes[i][j]){
+                    puzzle[i][j] = new Box(false, 0);
+                } else {
+                    puzzle[i][j] = new Box(true, 0);
+                }
             }
         }
     }
@@ -82,3 +96,14 @@ public class WordPuzzle {
         System.out.println(wp);
     }
 }
+
+/*Output
+ black  1wh  2wh black black black  3wh  4wh  5wh
+  6wh white white  7wh black  8wh white white white
+  9wh white white white  10wh white black black black
+  11wh white black  12wh white white black  13wh  14wh
+ black black black  15wh white white  16wh white white
+  17wh  18wh  19wh white black  20wh white white white
+  21wh white white black black black  22wh white black
+
+ */
