a7b38b9006b313c7bfb45ccbe8ae92a585e4cb78
Dh-Van <dhvan17shah@gmail.com>
2022-02-16 15:29:44 +0000

    hviushiud

---
+++ src/com/ch9/wordduo/WordDuo.java
@@ -0,0 +1,13 @@
+package com.ch9.wordduo;
+
+public class WordDuo {
+    String first;
+    String second;
+    /** Constructor **/
+    public WordDuo(String f, String s){
+        first = f;
+        second = s;
+    }
+    public String getFirst(){ return first;}
+    public String getSecond(){return second;}
+}
---
+++ src/com/ch9/wordduo/WordDuoList.java
@@ -0,0 +1,130 @@
+package com.ch9.wordduo;
+
+import java.util.ArrayList;
+
+public class WordDuoList {
+    private ArrayList<WordDuo> allDuos;
+
+
+    /*
+    Write the constructor for the WordDuoList class.  The constructor
+    takes an array of Strings as a parameter and initializes the
+    instance variable allDuos to an ArrayList of WordDuo objects.
+
+    A WordDuo object consists of a word from the array paired with a
+    word that appears later in the array.  The allDuos list constins
+    WordDuo objects (words[i], words[j]) for every i and j, where
+    0<=i<j<words.length.  Each WordDuo object is added exactly once to
+    the list.
+
+    The following examples illustrate two different WordDuo objects.
+
+    Example 1:
+    String [] wordNums = {"one", "two", "three"};
+    WordDuoList exampleOne = new WordDuoList(wordNums);
+
+    After the code has executed, the allDuos instance variable
+    of exampleOne will contain the following WordDuo objects in the
+    same order.
+    ("one", "two"), ("one", "three"), ("two", "three")
+
+    Example 2:
+    String[] phrase = {"the", "more", "the", "merrier"};
+    WordDuoList exampleTwo = new WordDuoList(phrase);
+
+    After the code segment has executed, the allDuos instance variable of
+    exampleTwo will contain the following WordDuo objects in come order.
+    ("the", "more"), ("the", "the"), ("the", "merrier"),
+    ("more", "the"), ("more", "merrier"), ("the", "merrier")
+     */
+    /** Constructor
+     * Precondition:  word.length >2
+     */
+    public WordDuoList(String[] words){
+        /* part A */
+        allDuos = new ArrayList<>();
+		for(int i = 1, counter = 0; counter < words.length - 2; i++){
+            i = (i == words.length) ? (i = 1 + ++counter) : i;
+            allDuos.add(new WordDuo(words[counter], words[i]));
+        }
+		
+
+    }
+    public String toString(){
+        String s = "";
+        for (WordDuo wd: allDuos){
+            s += "("+wd.getFirst() + ", " + wd.getSecond() + ")\n";
+        }
+        return s;
+    }
+
+    /* Write the WordDuoList method numMatches.  This method returns the number
+    of WordDuo objects in allDuos for which the two strings match.
+
+    For example, the following code segment creates a WordDuoList object.
+
+    String[] moreWords = {"the", "red", "fox", "the", "red");
+    WordDuoList exampleThree = new WordDuoList(moreWords);
+
+    After the code segment has executed, the allDuos instance variable of
+    exampleThree will contain the following WordDuo objects in some order.
+    The matching pairs are starred **.
+
+    ("the", "red"), ("the", "fox"), **("the", "the")**, ("the", "red")
+    ("red", "fox"), ("red", "the"), **("red", "red")**, ("fox", "the")
+    ("fox", "red"), ("the", "red")
+     */
+    public int numMatches(){
+        int retNum = 0;
+
+        for(WordDuo wD : allDuos){
+            retNum += (wD.first.equals(wD.second)) ? 1 : 0;    
+        }
+
+        return retNum;
+    }
+
+    /*Write the method moveMatchesToTop()  This method will look for
+    WordDuo matches and move them to the beginning of the ArrayList.
+    For example, the list above would end up as follows.  The relative
+    order of these matches does not matter.
+
+    ("red", "red"), ("the", "the"),("the", "red"), ("the", "fox"),
+    ("the", "red") , ("red", "fox"), ("red", "the"), ("fox", "the")
+    ("fox", "red"), ("the", "red")
+
+     */
+    public void moveMatchesToTop(){
+        /* part c */
+        for(int i = 0; i < allDuos.size(); i++){
+            WordDuo wD = allDuos.get(i);
+            if(wD.first.equals(wD.second)){
+                allDuos.remove(i);
+                allDuos.set(0, wD);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        String [] stuff = {"the", "red", "fox", "the", "red"};        
+        WordDuoList wdl = new WordDuoList(stuff);
+        System.out.println(wdl);
+        System.out.println(wdl.numMatches());
+        wdl.moveMatchesToTop();
+        System.out.println(wdl);
+
+        // String [] stuff2 = {"one","fish","two","fish","red","fish","blue","fish"};
+        // WordDuoList wdl2 = new WordDuoList(stuff2);
+        // System.out.println(wdl2);
+        // System.out.println(wdl2.numMatches());
+        // wdl2.moveMatchesToTop();
+        // System.out.println(wdl2);
+
+        // String [] stuff3 = {"call","me","ishmael"};
+        // WordDuoList wdl3 = new WordDuoList(stuff3);
+        // System.out.println(wdl3);
+        // System.out.println(wdl3.numMatches());
+        // wdl3.moveMatchesToTop();
+        // System.out.println(wdl3);
+    }
+}
