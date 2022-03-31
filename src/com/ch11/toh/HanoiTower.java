e1c25ec70ee6a6e309ac806444b02f4e4e779624
dh-van <dhvan17shah@gmail.com>
2022-03-31 12:03:56 -0400

    ,,

---
+++ src/com/ch11/toh/HanoiTower.java
@@ -0,0 +1,32 @@
+package com.ch11.toh;
+
+public class HanoiTower {
+    private Peg peg1;
+    private Peg peg2;
+    private Peg peg3;
+    private int numDiscs;
+
+    public HanoiTower(int num)
+    {
+        this.numDiscs = num;
+        this.peg1 = new Peg(1, numDiscs);
+        this.peg2 = new Peg(2, numDiscs);
+        this.peg3 = new Peg(3, numDiscs);
+
+    }
+    public void solveTower()
+    {
+        moveTower(peg1, peg3, peg2, numDiscs);
+    }
+
+    private void moveTower(Peg startPeg, Peg endPeg, Peg extraPeg, int numtoMove)
+    {
+        
+    }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        HanoiTower ht = new HanoiTower(5);
+        ht.solveTower();
+    }
+
+}
---
+++ src/com/ch11/toh/Peg.java
@@ -0,0 +1,82 @@
+package com.ch11.toh;
+
+import java.util.Arrays;
+
+public class Peg {
+    private int pegNum;
+    private int numDiscs;  // The total number of discs in the current game.
+    private int[] discs;   // array of discs - bottom disc is in position 0
+    // the discs are integers which represent their size
+    private int count;     // the number of discs currently on the peg.
+
+    public Peg(int peg, int num)
+    {
+        this.pegNum = peg;
+        this.numDiscs = num;
+
+        discs = new int[numDiscs];
+
+        count = 0;
+    }
+
+    public int getPegNum()
+    {
+        return pegNum;
+    }
+
+    public String toString()
+    {
+        String s = "Peg " + pegNum;
+        return s;
+    }
+    public boolean addDisc(int size)
+    {
+        if(count == 0 || discs[count-1] > size){
+            discs[count] = size;
+            count++;
+            return true;
+        } else {
+            System.out.println("Can't do that");
+            return false;
+        }
+    }
+    public int popDisc()
+    {
+        int size = discs[count];
+        discs[count-1] = 0;
+        return size;
+    }
+    public boolean moveTopDisc(Peg newPeg)
+    {
+        int disc = popDisc();
+        if (newPeg.addDisc(disc))
+        {
+            System.out.println("Moving disc " + disc + " from peg " + pegNum + " to peg " + newPeg.getPegNum());
+            return true;
+        }
+        else{
+            System.out.println("Bad Move");
+            return false;
+        }
+    }
+    public static void main(String [] arg) {
+        Peg p1 = new Peg(1,5);
+        p1.addDisc(3);
+        System.out.println(Arrays.toString(p1.discs));
+        p1.addDisc(2);
+        System.out.println(Arrays.toString(p1.discs));
+        p1.addDisc(4);
+        System.out.println(Arrays.toString(p1.discs));
+        p1.popDisc();
+        System.out.println(Arrays.toString(p1.discs));
+    }
+
+
+}
+/**
+ [3, 0, 0, 0, 0]
+ [3, 2, 0, 0, 0]
+ Can't do that
+ [3, 2, 0, 0, 0]
+ [3, 0, 0, 0, 0]
+ **/
