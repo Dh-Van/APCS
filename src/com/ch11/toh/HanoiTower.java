69ff8f8763d1281007186215cff41bbdc8ffcde7
Dh-Van <dhvan17shah@gmail.com>
2022-04-03 02:48:39 +0000

    idk anymore

--- src/com/ch11/toh/HanoiTower.java
+++ src/com/ch11/toh/HanoiTower.java
@@ -6,26 +6,74 @@ public class HanoiTower {
     private Peg peg3;
     private int numDiscs;
 
-    public HanoiTower(int num)
-    {
+    public HanoiTower(int num) {
         this.numDiscs = num;
         this.peg1 = new Peg(1, numDiscs);
         this.peg2 = new Peg(2, numDiscs);
         this.peg3 = new Peg(3, numDiscs);
 
+        for(int i = numDiscs; i > 0; i--){
+            peg1.addDisc(i);
+        }
+
     }
-    public void solveTower()
-    {
+
+    public void solveTower() {
         moveTower(peg1, peg3, peg2, numDiscs);
     }
 
-    private void moveTower(Peg startPeg, Peg endPeg, Peg extraPeg, int numtoMove)
-    {
-        
+    private void moveTower(Peg pegA, Peg pegB, Peg pegC, int numtoMove) {
+        create6Stack(pegA, pegC, pegB);
+        System.out.println(pegA.getDiscs());
+        System.out.println(pegB.getDiscs());
+        System.out.println(pegC.getDiscs());
     }
+
+    /**
+     * 
+     * @param origin The peg with the discs in the two stack
+     * @param destination The peg where two stack should be created
+     * @param intermediate The extra peg
+     */
+    private void create2Stack(Peg origin, Peg destination, Peg intermediate){
+        // Moves the top disc from a to b
+        origin.moveTopDisc(intermediate);
+        // Moves the top disc from a to c
+        origin.moveTopDisc(destination);
+        // Moves the top disc from b to c
+        intermediate.moveTopDisc(destination);
+    }
+
+    private void create3Stack(Peg origin, Peg destination, Peg intermediate){
+        // Creates a 2 stack from a to b
+        create2Stack(origin, intermediate, destination);
+        // Moves the top disc from a to c
+        origin.moveTopDisc(destination);
+        // Creates a 2 stack from b to c
+        create2Stack(intermediate, destination, origin);
+    }
+
+    private void create4Stack(Peg origin, Peg destination, Peg intermediate){
+        create3Stack(origin, intermediate, destination);
+        origin.moveTopDisc(destination);
+        create3Stack(intermediate, destination, origin);
+    }
+
+    private void create5Stack(Peg origin, Peg destination, Peg intermediate){
+        create4Stack(origin, intermediate, destination);
+        origin.moveTopDisc(destination);
+        create4Stack(intermediate, destination, origin);
+    }
+
+    private void create6Stack(Peg origin, Peg destination, Peg intermediate){
+        create5Stack(origin, intermediate, destination);
+        origin.moveTopDisc(destination);
+        create5Stack(intermediate, destination, origin);
+    }
+    
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        HanoiTower ht = new HanoiTower(5);
+        HanoiTower ht = new HanoiTower(6);
         ht.solveTower();
     }
 
--- src/com/ch11/toh/Peg.java
+++ src/com/ch11/toh/Peg.java
@@ -9,8 +9,7 @@ public class Peg {
     // the discs are integers which represent their size
     private int count;     // the number of discs currently on the peg.
 
-    public Peg(int peg, int num)
-    {
+    public Peg(int peg, int num) {
         this.pegNum = peg;
         this.numDiscs = num;
 
@@ -19,18 +18,21 @@ public class Peg {
         count = 0;
     }
 
-    public int getPegNum()
-    {
+    public int getPegNum() {
         return pegNum;
     }
 
-    public String toString()
-    {
+    public String getDiscs(){
+        return Arrays.toString(discs);
+    }
+
+    public String toString() {
         String s = "Peg " + pegNum;
         return s;
     }
-    public boolean addDisc(int size)
-    {
+
+    // Adds a disc of a given size to the curent peg
+    public boolean addDisc(int size) {
         if(count == 0 || discs[count-1] > size){
             discs[count] = size;
             count++;
@@ -40,27 +42,38 @@ public class Peg {
             return false;
         }
     }
-    public int popDisc()
-    {
-        int size = discs[count];
-        discs[count-1] = 0;
+
+    // Gets the top most disc from the peg and removes it
+    public int popDisc() {
+        int size = discs[count - 1];
+        discs[count - 1] = 0;
+        count--;
         return size;
     }
-    public boolean moveTopDisc(Peg newPeg)
-    {
+
+    public int getNumDiscs(){
+        return count;
+    }
+
+    // Moves the top disc from the current peg to another peg
+    public boolean moveTopDisc(Peg newPeg) {
         int disc = popDisc();
-        if (newPeg.addDisc(disc))
-        {
+        if (newPeg.addDisc(disc)) {
             System.out.println("Moving disc " + disc + " from peg " + pegNum + " to peg " + newPeg.getPegNum());
             return true;
         }
-        else{
+        else {
             System.out.println("Bad Move");
+            // Need to add the popped disc if the move wasn't legal 
+            // this.addDisc(disc);
             return false;
         }
     }
+
     public static void main(String [] arg) {
         Peg p1 = new Peg(1,5);
+        Peg p2 = new Peg(2,5);
+
         p1.addDisc(3);
         System.out.println(Arrays.toString(p1.discs));
         p1.addDisc(2);
@@ -69,10 +82,16 @@ public class Peg {
         System.out.println(Arrays.toString(p1.discs));
         p1.popDisc();
         System.out.println(Arrays.toString(p1.discs));
+
+        p1.moveTopDisc(p2);
+        System.out.println(Arrays.toString(p2.discs));
     }
 
 
+
+
 }
+
 /**
  [3, 0, 0, 0, 0]
  [3, 2, 0, 0, 0]
